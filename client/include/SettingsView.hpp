fb1be21fe76563b92e306daaa29a839615188691
Mathieu Rio <mathieu.rio@epitech.eu>
2023-12-11 09:26:42 +0100

    feat: add header

---
+++ client/include/SettingsView.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPP-500-REN-5-2-rtype-tom.daniel
+** File description:
+** SettingsView
+*/
+
+#ifndef SETTINGSVIEW_HPP_
+#define SETTINGSVIEW_HPP_
+
+class SettingsView {
+    public:
+        SettingsView();
+        ~SettingsView();
+
+    protected:
+    private:
+};
+
+#endif /* !SETTINGSVIEW_HPP_ */
+
